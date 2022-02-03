e2c604a3d6dc5d1c82896a39b4874b3bbe7af8e7
Eric Rustrum <48301203+MadKamel@users.noreply.github.com>
2022-02-03 09:54:54 -0800

    Bugfixes, changed weapon ranges.
    The basher will now hit farther than the zapper.

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ minetest.register_tool("xenozapper:zapper", {
 	description = "Xeno-Zapper",
 	inventory_image = "zapper.png",
 	wield_image = "zapper.png^[transformR90",
+	range = 3,
 	tool_capabilities = {
 		full_punch_interval = 0.01,
 		max_drop_level=1,
@@ -24,7 +25,8 @@ minetest.register_tool("xenozapper:basher", {
 	description = "Xeno-Basher",
 	inventory_image = "basher_inventory.png",
 	wield_image = "basher.png",
-	wield_scale = {x=2,y=2,z=2},
+	wield_scale = {x=2,y=2,z=1},
+	range = 5,
 	tool_capabilities = {
 		full_punch_interval = 0.1,
 		max_drop_level=1,
